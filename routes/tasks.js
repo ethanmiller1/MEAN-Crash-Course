3d3d607ce5fe874aba053772028b810f3c403a2f
Ethan Miller <ethan.romans5.8@gmail.com>
2019-08-26 08:42:21 -0500

    get single task

--- routes/tasks.js
+++ routes/tasks.js
@@ -3,12 +3,23 @@ let router = express.Router();
 let mongojs = require('mongojs');
 let db = mongojs('mongodb://localhost:27017/mytasklist');
 
+// Get all tasks.
 router.get('/tasks', function(req, res, next){
   db.tasks.find(function(err, tasks){
     if(err){
       res.send(err);
     }
     res.json(tasks);
+  });
+});
+
+// Get single task.
+router.get('/tasks/:id', function(req, res, next){
+  db.tasks.findOne({_id: mongojs.ObjectId(req.params.id)}, function(err, task){
+    if(err){
+      res.send(err);
+    }
+    res.json(task);
   });
 });
 
